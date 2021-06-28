b92449690732e17ee9ed449b57186e0781560483
Mike Urich <michaelurich210@gmail.com>
2021-06-27 21:27:36 -0400

    merged notify into applepay file

--- applepay.js
+++ applepay.js
@@ -2,10 +2,8 @@ var ApplePaySession = {
 	canMakePayments: function () {
 		console.log('canMakePayments');
 		// alert('This website supports Apple Pay')
-		//I hope that it will only run when sites look for this and not on every site regardless of if it uses the element
-		//This is a placeholder for whatever we'll do to actually alert the user in a normal way
-		// alertApplePay();
-		browser.runtime.sendMessage()
+		alertApplePay();
+		// browser.runtime.sendMessage()
 		return Promise.resolve(true); // if notifications work then return false here instead
 	},
 	canMakePaymentsWithActiveCard: function () {
@@ -18,16 +16,16 @@ var ApplePaySession = {
 	},
 };
 
-// function alertApplePay() {
-// 	console.log('Im trying to notify');
-// 	browser.notifications.create({
-// 		"type": "basic",
-// 		"iconUrl": browser.extension.getURL("icons/apple-wallet-48.png"),
-// 		"title": "Apple Pay Supported",
-// 		"message": "This website is trying to use Apple Pay."
-// 	});
-// 	console.log('I should have notified you');
-// }
+function alertApplePay() {
+	console.log('Im trying to notify');
+	browser.notifications.create({
+		"type": "basic",
+		"iconUrl": browser.extension.getURL("icons/apple-wallet-48.png"),
+		"title": "Apple Pay Supported",
+		"message": "This website is trying to use Apple Pay."
+	});
+	console.log('I should have notified you');
+};
 
 window.wrappedJSObject.ApplePaySession = cloneInto(
 	ApplePaySession,
