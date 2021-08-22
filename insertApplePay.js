cd1e4340656e06720b894381a55bca617df828ac
Mike Urich <michaelurich210@gmail.com>
2021-08-22 16:01:52 -0400

    Adding todos and cleaning up. Still need to figure out copy on click

--- insertApplePay.js
+++ insertApplePay.js
@@ -1,20 +1,13 @@
 var ApplePaySession = {
 	canMakePayments: function () {
-		console.log('canMakePayments');
 		notify("Click to copy URL for Safari");
 		copyToClipboard(window.location.href);
-		// browser.runtime.sendMessage({"url": e.target.href}); //pass info to background script here
-		// return Promise.resolve(true); // if notifications work then return false here instead
 		return Promise.resolve(false);
 	},
 	canMakePaymentsWithActiveCard: function () {
-		// console.log('withActiveCard');
-		// return Promise.resolve(true);
 		return Promise.resolve(false);
 	},
 	supportsVersion: function () {
-		// console.log('supportsVersion');
-		// return Promise.resolve(true);
 		return Promise.resolve(false);
 	},
 };
