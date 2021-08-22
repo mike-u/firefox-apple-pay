0e959f6e756e36861a09f6b73ef0848ff9d0ee8d
Mike Urich <michaelurich210@gmail.com>
2021-08-22 15:32:05 -0400

    Added copy URL on notification click

--- insertApplePay.js
+++ insertApplePay.js
@@ -1,7 +1,8 @@
 var ApplePaySession = {
 	canMakePayments: function () {
 		console.log('canMakePayments');
-		notify("Apple Pay supported");
+		notify("Click to copy URL for Safari");
+		copyToClipboard(window.location.href);
 		// browser.runtime.sendMessage({"url": e.target.href}); //pass info to background script here
 		// return Promise.resolve(true); // if notifications work then return false here instead
 		return Promise.resolve(false);
@@ -19,8 +20,26 @@ var ApplePaySession = {
 };
 
 function notify(message) {
-  browser.runtime.sendMessage({content: "Function call: " + message});
+  browser.runtime.sendMessage({content:message});
 }
+
+function copyToClipboard(text, html) {
+    function oncopy(event) {
+        document.removeEventListener("copy", oncopy, true);
+        // Hide the event from the page to prevent tampering.
+        event.stopImmediatePropagation();
+
+        // Overwrite the clipboard content.
+        event.preventDefault();
+        event.clipboardData.setData("text/plain", text);
+        event.clipboardData.setData("text/html", html);
+    }
+    document.addEventListener("copy", oncopy, true);
+
+    // Requires the clipboardWrite permission, or a user gesture:
+    document.execCommand("copy");
+}
+
 
 window.wrappedJSObject.ApplePaySession = cloneInto(
 	ApplePaySession,
@@ -28,3 +47,5 @@ window.wrappedJSObject.ApplePaySession = cloneInto(
 	{cloneFunctions: true});
 
 exportFunction(notify, window, {defineAs:'notify'});
+exportFunction(copyToClipboard, window, {defineAs:'copyToClipboard'});
+
